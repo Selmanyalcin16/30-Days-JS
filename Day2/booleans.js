3b04d371b28cad98fa65ed39d8a5bc60767b2ec0
0xVonax <102980368+0xVonax@users.noreply.github.com>
2024-08-08 11:53:35 +0300

    Day2 JS

---
+++ Day2/booleans.js
@@ -0,0 +1,11 @@
+let isRaining = false;
+
+isRaining ? console.log("it is raining") : console.log("it is not raining"); //Tek satırda if else kullanımı baya iyi özellik
+
+///Time fonksiyonları
+
+const now = new Date();
+console.log(now);
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getFullYear());
